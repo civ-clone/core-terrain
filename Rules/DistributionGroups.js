ce0f169453cc59c9e9fd777f96c9d7758bf43134
dom111 <dom111@users.noreply.github.com>
2021-02-17 10:46:33 +0000

    Remove unused `Rule`s.

--- Rules/DistributionGroups.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DistributionGroups = void 0;
-const Rule_1 = require("@civ-clone/core-rule/Rule");
-class DistributionGroups extends Rule_1.default {
-}
-exports.DistributionGroups = DistributionGroups;
-exports.default = DistributionGroups;
-//# sourceMappingURL=DistributionGroups.js.map
